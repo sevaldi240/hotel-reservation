0973f88bd745892e196c7fb86604cbbd300065f4
sevaldi240 <sebastian_guaman26@hotmail.com>
2024-02-28 03:05:48 -0500

    regreso

--- src/app/(web)/page.tsx
+++ src/app/(web)/page.tsx
@@ -12,8 +12,8 @@ const Home = async () =>{
     <HeroSection/>
     <PageSearch/>
     <FeaturedRoom featuredRoom={featuredRoom}/>
-    <Gallery/>
-    <NewsLetter/>
+    {/* <Gallery/>
+    <NewsLetter/> */}
   </>
   );
 };
