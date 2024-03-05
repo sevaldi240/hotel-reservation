d8da9cf19240e61d8b2473238dc265707b07f913
sevaldi240 <sebastian_guaman26@hotmail.com>
2024-03-05 16:46:11 -0500

    Change

--- src/components/BookRoomCtaModal/BookRoomCtaModal.tsx
+++ src/components/BookRoomCtaModal/BookRoomCtaModal.tsx
@@ -238,13 +238,13 @@ const BookRoomCtaModal: FC<Props> = props => {
             {isSubmittingBooking ? "Submitting" : "Submit"}
       </button> */}
       <button
-            onClick={handleBookNowClicks}
+            onClick={toggleBookingModal}
             className="ml-2 px-4 py-2 bg-gray-300 text-gray-700 rounded-md hover:bg-gray-400"
           >
             Cancel
       </button>
       <button
-        onClick={async () => {toggleBookingModal();
+        onClick={async () => {handleBookNowClicks();
           handleUpdateBooking();
           const res = await fetch('/api/sendt', 
           {
